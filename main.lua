e124c3627ef21394388fd36af6f68707c539abb4
notyetawizard <notyetawizard@users.noreply.github.com>
2016-03-10 08:56:29 -0600

    I totally know what I'm doing.

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
---Can't just use os.execute, as that doesn't return the output!
-winlist = io.popen("lsw",r)
-print(winlist:read("*a"))
+--[[
+TODO:
+  Set up handling for all events wew sends and nil, even if they're just a return.
+  Window focus on mouse enter event? (7)
+--]]
+
+
+--Example: Can't just use os.execute, as that doesn't return the output!
+--winlist = io.popen("lsw",r)
+--print(winlist:read("*a"))
+
+--Event handler?
+events = io.popen("wew",r)
+--read line by line in a loop, but *don't* skip lines accidentally!
+--do things :D
